5ee842807c79fd6dd7f01421503f99bc2dd10f6c
lkwg82@gmx.de <lkwg82@gmx.de@c69c990e-1be5-11de-9d52-39b120432c5d>
2009-03-29 16:08:35 +0000

    * checked and modified with perl::critic and perltidy

--- trunk/sshc.pl
+++ trunk/sshc.pl
@@ -1,254 +1,340 @@
 #!/bin/perl
 
-$|=1;
-
-
-use diagnostics;
 use strict;
 use warnings;
+use diagnostics -verbose;
+
+# perltidy --backup-and-modify-in-place -l=120 -cti=2  --opening-brace-on-new-line --opening-sub-brace-on-new-line -lp -vt=0 -vtc=0 -cab=0 sshc.pl
+
+BEGIN
+{
+    $diagnostics::DEBUG  = 0;
+    $diagnostics::PRETTY = 1;
+
+    if (0)
+    {
+        use Perl::Critic;
+        my $file = $0;
+        my $critic = Perl::Critic->new( -severity => '3',
+                                        -verbose  => 11 );
+        my @violations = $critic->critique($file);
+
+        my ( $i, $n ) = ( 0, scalar @violations );
+        for ( reverse Perl::Critic::Violation::sort_by_severity(@violations) )
+        {
+            print '-' x 20, "\n";
+            printf( "%3d/%d ", ++$i, $n );
+            print "[" . $_->severity() . "] " . $_;
+            print " -- source --- \n";
+            print " source: ", $_->source(), "\n";
+            print " -- /source --- \n";
+            print " expl: ", $_->explanation(), "\n";
+            print " desc: ", $_->description(), "\n";
+            print " dia : ", $_->diagnostics(), "\n";
+            print "\n --- press Enter to continue ---\n";
+            <STDIN>;
+        }
+        exit;
+    }
+}
 
 use Data::Dumper;
-
-my $ssh_config  = &read_ssh_config;
-
-#exit;
+use autodie qw< :io >;
+use English qw(-no_match_vars);
+$OUTPUT_AUTOFLUSH = 1;
+my $ssh_config = &read_ssh_config;
 
 use Curses::UI;
-my $cui = Curses::UI->new(
-	-color_support,-clear_on_exit,-mouse_support
-);
+my $cui = Curses::UI->new();
+
+#-color_support,-clear_on_exit,-mouse_support
+#);
 
 my $w = $cui->width();
 my $h = $cui->height();
 
-sub exit_dialog() {
-	my $return = $cui->dialog(
-		-message => "Do you really want to quit?",
-		-title   => "Are you sure???",
-		-buttons => [ 'yes', 'no' ],
-		-border  => 1,
+sub exit_dialog
+{
+    my $return = $cui->dialog(
+        -message => 'Do you really want to quit?',
+        -title   => 'Are you sure???',
+        -buttons => [ 'yes', 'no' ],
+        -border  => 1,
 
-	);
+        );
 
-	exit(0) if $return;
+    exit 0 if $return;
+
+    return;
 }
 
 my %options = (
-	-title  => 'profile',
-	-width  => $w / 3,
-	-height => $h,
-	-border => 1,
-	-padtop => 3,
-);
+                -title  => 'profile',
+                -width  => $w / 3,
+                -height => $h,
+                -border => 1,
+                -padtop => 3,
+                );
 
 my %options_info = (
-	-title  => 'infos',
-	-width  => $w,
-	-height => 3,
-	-border => 1,
-);
+                     -title  => 'infos',
+                     -width  => $w,
+                     -height => 3,
+                     -border => 1,
+                     );
 
 my %options2 = (
-	-title   => 'details',
-	-width   => $w,
-	-height  => $options{-height},
-	-border  => 1,
-	-padleft => $w - $options{-width} * 2,
-	-padtop  => $options{-padtop}
-);
+                 -title   => 'details',
+                 -width   => $w,
+                 -height  => $options{-height},
+                 -border  => 1,
+                 -padleft => $w - $options{-width} * 2,
+                 -padtop  => $options{-padtop},
+                 );
 
 my $win      = $cui->add( 'window_id',   'Window', %options );
 my $win_info = $cui->add( 'window_info', 'Window', %options_info );
 
-$cui->set_binding( sub { \&exit_dialog(); }, "q" );
+$cui->set_binding( sub { \&exit_dialog(); }, 'q' );
 
 my $label = $win_info->add(
-	'mylabel', 'Label',
-	-text => 'quit: q',
-	-bold => 0,
-)->draw();
+                            'mylabel', 'Label',
+                            -text => 'quit: q',
+                            -bold => 0,
+                            )->draw();
 
 my $label2 = $win_info->add(
-	'mylabel1', 'Label',
-	-text => 'Hello, world!\nds',
-	-bold => 0,
-	-x    => 30
-)->draw();
+                             'mylabel1', 'Label',
+                             -text => 'Hello, world!\nds',
+                             -bold => 0,
+                             -x    => 30
+                             )->draw();
 
-
-
-my @values = sort { lc($a) cmp lc($b) } ( keys %{$ssh_config} );
+my @values = sort { lc $a cmp lc $b } ( keys %{$ssh_config} );
 
 my $listbox = $win->add(
-	'mylistbox', 'Listbox',
-	-values => \@values,
+    'mylistbox', 'Listbox',
+    -values => \@values,
 
-	#-labels => \%labels,
+    #-labels => \%labels,
 
-	#~ -radio     => 1,
-	-onselchange => sub { \&display_ssh_config($ssh_config); }
-);
+    #~ -radio     => 1,
+    -onselchange => sub { \&display_ssh_config($ssh_config); }
+    );
 
 $listbox->focus();
 $cui->mainloop();
 
-sub CURSE_setProperty{
-	my ($obj,$property,$value) = @_;
+sub CURSE_setProperty
+{
+    my ( $obj, $property, $value ) = @_;
 
-	if ($obj)
-	{
-		#~ if ( exists($ob->{$property}) )
-		{
-			$obj->{$property} = $value;
+    if ($obj)
+    {
 
-			$obj->draw();
-		}
-		#~ else
-		#~ {
-			#~ $cui->error(" \$obj has not such property : $property ");
-		#~ }
-	}
-	else
-	{
-		$cui->error("no such obj");
-	}
+        #~ if ( exists($ob->{$property}) )
+        {
+            $obj->{$property} = $value;
+
+            $obj->draw();
+        }
+
+        #~ else
+        #~ {
+        #~ $cui->error(" \$obj has not such property : $property ");
+        #~ }
+    }
+    else
+    {
+        $cui->error('no such obj');
+    }
+
+    return;
 }
 
-sub display_ssh_config {
-	my $config = shift;
-	my $key    = $win->getobj('mylistbox')->get_active_value();
+sub display_ssh_config
+{
+    my $config = shift;
+    my $key    = $win->getobj('mylistbox')->get_active_value();
 
-	my $win2 = $cui->add( 'w2', 'Window', %options2 )->draw();
-	my $offSet = { x => 0, y => 0 };
+    my $win2 = $cui->add( 'w2', 'Window', %options2 )->draw();
+    my $offSet = {
+                   x => 0,
+                   y => 0
+                   };
 
-	# $key		- of the hash with the dataset
-	# $offset	- remember the postions of the labels
-	# $value_key	- the config-key
-	# $default_value- if the config-key not exists display this instead [optional]
-	my $subFillLabels = sub {
-		my ($key,$offSet, $value_key, $default_value) = @_;
+    # $key        - of the hash with the dataset
+    # $offset    - remember the postions of the labels
+    # $value_key    - the config-key
+    # $default_value- if the config-key not exists display this instead [optional]
+    my $subFillLabels = sub {
+        my ( $key_, $_offSet, $value_key, $default_value ) = @_;
 
-		my ( $label_key, $label_val ) = ( 'label_k_' . $value_key, 'label_v_' . $value_key );
-		my $x_offset = 20;
+        my ( $label_key, $label_val ) = ( 'label_k_' . $value_key, 'label_v_' . $value_key );
+        my $x_offset = 20;
 
-		my $entry = $config->{$key};
+        my $entry = $config->{$key_};
 
-		if ( exists($entry->{$value_key}) || $default_value )
-		{
-			my $init_obj = sub {
-				my ($key,$options) = @_;
+        if ( exists $entry->{$value_key} || $default_value )
+        {
+            my $init_obj = sub {
+                my ( $_key, $options ) = @_;
 
-				unless ( $win2->getobj($key) )
-				{
-					$win2->add( @{$options} )->draw();
-				}
-			};
+                if ( !$win2->getobj($_key) )
+                {
+                    $win2->add( @{$options} )->draw();
+                }
+            };
 
-			$init_obj->(	$label_key,
-					[
-						$label_key, 'Label',
-						-text 	=> $value_key,
-						-bold 	=> 1,
-						-x	=> $offSet->{x},
-						-y	=> $offSet->{y}
-					]
-			);
+            $init_obj->(
+                         $label_key,
+                         [
+                            $label_key, 'Label',
+                            -text => $value_key,
+                            -bold => 1,
+                            -x    => $_offSet->{x},
+                            -y    => $_offSet->{y}
+                         ]
+                         );
 
-			my $value 	 = exists($entry->{$value_key}) ? $entry->{$value_key} : $default_value;
-			my @value_labels = ( ref($value) eq 'ARRAY' ) ? @{$value} : ($value);
+            my $value = exists $entry->{$value_key} ? $entry->{$value_key} : $default_value;
+            my @value_labels = ( ref $value eq 'ARRAY' ) ? @{$value} : ($value);
 
-			for(my $i=0; $i<scalar(@value_labels); $i++)
-			{
-				my $label = $label_val.'_'.$i;
-				$init_obj->(	$label,
-						[
-							$label, 'Label',
-							-bold	=> 0,
-							-x	=> $x_offset,
-							-y	=> $offSet->{y},
-							-text	=> $value_labels[$i]
-						]
-				);
+            for my $i ( 0 .. scalar @value_labels )
+            {
+                my $_label = $label_val . '_' . $i;
+                $init_obj->(
+                             $label,
+                             [
+                                $_label, 'Label',
+                                -bold => 0,
+                                -x    => $x_offset,
+                                -y    => $_offSet->{y},
+                                -text => $value_labels[$i]
+                             ]
+                             );
 
-				$offSet->{y} +=  $win2->getobj($label)->{-height};
-			}
-		}
+                $_offSet->{y} += $win2->getobj($_label)->{-height};
+            }
+        }
 
-	};
+    };
 
+    $cui->leave_curses();
+    $subFillLabels->( ( $key, $offSet ), ( 'Hostname', $key ) );
+    $subFillLabels->( ( $key, $offSet ), ('User') );
+    $subFillLabels->( ( $key, $offSet ), ('Port') );
+    $subFillLabels->( ( $key, $offSet ), ('IdentityFile') );
+    $subFillLabels->( ( $key, $offSet ), ('Compression') );
+    $subFillLabels->( ( $key, $offSet ), ('LocalForward') );
+    $subFillLabels->( ( $key, $offSet ), ('RemoteForward') );
 
-	$cui->leave_curses();
-	$subFillLabels->( ($key,$offSet),('Hostname',$key));
-	$subFillLabels->( ($key,$offSet),('User'));
-	$subFillLabels->( ($key,$offSet),('Port'));
-	$subFillLabels->( ($key,$offSet),('IdentityFile'));
-	$subFillLabels->( ($key,$offSet),('Compression'));
-	$subFillLabels->( ($key,$offSet),('LocalForward'));
-	$subFillLabels->( ($key,$offSet),('RemoteForward'));
+    #$cui->destroy();
 
-	#$cui->destroy();
+    #print Dumper({$key=>$config->{$key}});
 
-	#print Dumper({$key=>$config->{$key}});
+    $cui->delete('w2');
 
-	$cui->delete('w2');
-
+    return;
 }
 
-sub read_ssh_config {
+sub read_ssh_config
+{
+    my $user = $ENV{'USER'};
 
-	my $user = $ENV{'USER'};
+    my %config = ();
 
-	my %config = ();
+    # temporary
+    my $host;
 
-	# temporary
-	my $host;
+    my $process_ssh_config = sub {
+        my ( $fh, $config ) = @_;
+        while (<$fh>)
+        {
+            ( local $_ = $_ ) =~ s/\n//smx;
 
-	open( CONFIG, "</home/" . $user . "/.ssh/config" ) || eval { die $^E; };
-	while (<CONFIG>) {
-		( local $_ = $_ ) =~ s/\n//;
+            if (
+                m{
+                    ^Host      # a comment
+                    [\ \t]      # or
+                    ([^#]+)      # empty line
+                }smxi
+              )
+            {
+                $host = $1;
+                $config->{$host} = ();
 
-		if (/^Host[\ \t]([^#]+)/i) {
-			$host = $1;
-			$config{$host} = ();
+                #print $host,"\n";
+            }
+            else
+            {
+                if (
+                    m{
+                            ^\#     # a comment
+                            |       # or
+                            ^$      # empty line
+                        }smx
+                  )
+                {
 
-			#print $host,"\n";
-		}
-		else {
-			if (/^#|^$/)
-			{
-				#print "-- Leerzeile\n";
-			}
-			else
-			{
-				/([^\ \t]+)[\ \t]([^#]+)/;
-				my ( $key, $value ) = ( $1, $2 );
-				if ( exists( $config{$host}{$key} ) )
-				{
-					if ( ref( $config{$host}{$key} ) eq 'ARRAY' )
-					{
-						push( @{ $config{$host}{$key} }, $value );
-					}
-					else
-					{
-						my $old_val = $config{$host}{$key};
-						$config{$host}{$key} = [ $old_val, $value ];
-					}
-				}
-				else
-				{
-					$config{$host}{$key} = $value;
+                    # ignore
+                }
+                else
+                {
+                    if (
+                        m{
+                            ([^\ \t]+)    # match everything except
+                            [\ \t]        # with space and tab in between
+                            ([^\#]+)        # match everthing except beginning comment
+                        }smx
+                      )
+                    {
+                        my ( $key, $value ) = ( $1, $2 );
+                        if ( exists $config->{$host}->{$key} )
+                        {
+                            if ( ref $config->{$host}->{$key} eq 'ARRAY' )
+                            {
+                                push @{ $config->{$host}->{$key} }, $value;
+                            }
+                            else
+                            {
+                                my $old_val = $config->{$host}->{$key};
+                                $config->{$host}->{$key} = [ $old_val, $value ];
+                            }
+                        }
+                        else
+                        {
+                            $config->{$host}->{$key} = $value;
+                        }
+                    }
+                    else
+                    {
+                        warn "regex not matched \n";
+                    }
+                }
+            }
 
-				}
+            #print $_ if (/^$/);
+        }
+    };
 
-			}
-		}
+    my $error_on_open = open my $fh, q{<}, '/home/' . $user . '/.ssh/config';
+    if ($error_on_open)
+    {
+        die $EXTENDED_OS_ERROR;
+    }
+    else
+    {
+        $process_ssh_config->( $fh, \%config );
+        my $error_on_close = close $fh;
+        if ($error_on_close)
+        {
+            die $EXTENDED_OS_ERROR;
+        }
+    }
 
-		#print $_ if (/^$/);
-	}
-	close(CONFIG);
+    #print Dumper( \%config );
+    #exit;
 
-	#print Dumper( \%config );
-	#exit;
-
-	return \%config;
+    return \%config;
 }
