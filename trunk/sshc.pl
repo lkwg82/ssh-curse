1626e1998e5e1d05bff0ad9de0380da442228150
lkwg82@gmx.de <lkwg82@gmx.de@c69c990e-1be5-11de-9d52-39b120432c5d>
2009-03-29 19:51:48 +0000

    + login & encrypted keys supported

--- trunk/sshc.pl
+++ trunk/sshc.pl
@@ -38,13 +38,16 @@ BEGIN
     }
 }
 
+use XML::Dumper;
 use Data::Dumper;
 use autodie qw< :io >;
 use English qw(-no_match_vars);
 $OUTPUT_AUTOFLUSH = 1;
 my $ssh_config = &read_ssh_config;
 
+use Curses;
 use Curses::UI;
+
 my $cui = Curses::UI->new();
 
 #-color_support,-clear_on_exit,-mouse_support
@@ -116,13 +119,155 @@ my $listbox = $win->add(
     'mylistbox', 'Listbox',
     -values => \@values,
 
-    #-labels => \%labels,
-
-    #~ -radio     => 1,
+    #~ -labels => {'12designer-dev'=>'12'},
     -onselchange => sub { \&display_ssh_config($ssh_config); }
     );
-
+$listbox->set_binding( sub { &ssh_connect( $ssh_config, $listbox ) }, KEY_ENTER );
+$listbox->{-onselchange}->();
 $listbox->focus();
+
+sub ssh_connect
+{
+    my ( $config, $listbox ) = @_;
+
+    my $key = $listbox->get_active_value();
+
+    my $cmdline = &ssh_get_connection_info( $config, $key );
+    $cui->leave_curses();
+
+    print "executing ssh : $cmdline \n";
+    system($cmdline);
+
+}
+
+sub ssh_get_connection_info
+{
+    my ( $config, $key ) = @_;
+
+    if ( exists $config->{$key} )
+    {
+        my $entry   = $config->{$key};
+        my $cmdline = 'ssh ';
+
+        my $getValue = sub {
+            my ( $key, $lowercase, $defaultkey ) = @_;
+
+            if ( defined($lowercase) ? $lowercase : 1 )
+            {
+                $key = lc $key;
+                $defaultkey = defined $defaultkey ? lc $defaultkey : undef;
+            }
+
+            if ( exists $entry->{$key} )
+            {
+                return $entry->{$key};
+            }
+            elsif ( defined $defaultkey && exists $entry->{$defaultkey} )
+            {
+                return $entry->{$defaultkey};
+            }
+            else
+            {
+                return undef;
+            }
+        };
+
+        my $appendStr = sub {
+            my ( $key, $prefix, $lowercase, $defaultkey, $sub_ref ) = @_;
+
+            my $value = $getValue->( $key, $lowercase, $defaultkey );
+
+            if ( defined $defaultkey && !defined $value )
+            {
+                warn "value should not be null \n";
+            }
+
+            if ( defined $value )
+            {
+                if ( ref $value ne 'ARRAY' )
+                {
+                    $value = [$value];
+                }
+
+                if ( defined $sub_ref && ref $sub_ref eq 'CODE' )
+                {
+                    map { $_ = $sub_ref->($_); } @{$value};
+                }
+
+                if ( defined $prefix )
+                {
+                    my $result;
+                    grep { $result .= ' ' . $prefix . ' ' . $_; } @{$value};
+                    return ' ' . $result;
+                }
+            }
+
+            return '';
+        };
+
+        my $appendFlag = sub {
+            my ( $key, $trueValue, $caseinsensitive, $flag_on_true ) = @_;
+
+            my $value = $getValue->( $key, 1 );
+
+            if ( ref $trueValue ne 'ARRAY' )
+            {
+                $trueValue = [$trueValue];
+            }
+
+            if ( defined $value )
+            {
+                for my $val ( @{$trueValue} )
+                {
+                    if ($caseinsensitive)
+                    {
+                        return $flag_on_true if ( $val =~ /^$value$/i );
+                    }
+                    else
+                    {
+                        return $flag_on_true if ( $val =~ /^$value$/ );
+                    }
+                }
+            }
+
+            return '';
+        };
+
+        my $prepareForwards = sub {
+            local $_ = shift;
+            s/[\ \t]+/:/;
+            return $_;
+        };
+
+        $cmdline .= $appendFlag->( 'Compression', 'Yes', 1, '-C' );
+        $cmdline .= $appendStr->( 'user',          '-l', 1 );
+        $cmdline .= $appendStr->( 'identityfile',  '-i', 1 );
+        $cmdline .= $appendStr->( 'port',          '-p', 1 );
+        $cmdline .= $appendStr->( 'localforward',  '-L', 1, undef, $prepareForwards );
+        $cmdline .= $appendStr->( 'remoteforward', '-R', 1, undef, $prepareForwards );
+        $cmdline .= $appendStr->( 'Hostname', '', 1, 'Host' );
+
+        $cmdline =~ s/([\ ])+/$1/g;
+        return $cmdline;
+    }
+    else
+    {
+        return '';
+    }
+}
+
+#&dump($listbox);
+
+sub dump
+{
+    use XML::Dumper;
+    my $dump = new XML::Dumper;
+
+    # ===== Dump to a file
+    my $file = "dump.xml";
+    $dump->pl2xml( $_[0], $file );
+}
+
 $cui->mainloop();
 
 sub CURSE_setProperty
@@ -157,6 +302,11 @@ sub display_ssh_config
     my $config = shift;
     my $key    = $win->getobj('mylistbox')->get_active_value();
 
+    if ( $cui->getobj('w2') )
+    {
+        $cui->delete('w2');
+    }
+
     my $win2 = $cui->add( 'w2', 'Window', %options2 )->draw();
     my $offSet = {
                    x => 0,
@@ -169,6 +319,7 @@ sub display_ssh_config
     # $default_value- if the config-key not exists display this instead [optional]
     my $subFillLabels = sub {
         my ( $key_, $_offSet, $value_key, $default_value ) = @_;
+        $value_key = lc $value_key;
 
         my ( $label_key, $label_val ) = ( 'label_k_' . $value_key, 'label_v_' . $value_key );
         my $x_offset = 20;
@@ -200,7 +351,7 @@ sub display_ssh_config
             my $value = exists $entry->{$value_key} ? $entry->{$value_key} : $default_value;
             my @value_labels = ( ref $value eq 'ARRAY' ) ? @{$value} : ($value);
 
-            for my $i ( 0 .. scalar @value_labels )
+            for my $i ( 0 .. scalar @value_labels - 1 )
             {
                 my $_label = $label_val . '_' . $i;
                 $init_obj->(
@@ -233,8 +384,6 @@ sub display_ssh_config
 
     #print Dumper({$key=>$config->{$key}});
 
-    $cui->delete('w2');
-
     return;
 }
 
@@ -255,14 +404,14 @@ sub read_ssh_config
 
             if (
                 m{
-                    ^Host      # a comment
+                    ^(Host)      # a comment
                     [\ \t]      # or
                     ([^#]+)      # empty line
                 }smxi
               )
             {
-                $host = $1;
-                $config->{$host} = ();
+                $host = $2;
+                $config->{$host} = { lc "$1" => $host };
 
                 #print $host,"\n";
             }
@@ -289,7 +438,10 @@ sub read_ssh_config
                         }smx
                       )
                     {
-                        my ( $key, $value ) = ( $1, $2 );
+
+                        # coz the keys are case-insensitive - lower them all
+                        my ( $key, $value ) = ( lc $1, $2 );
+
                         if ( exists $config->{$host}->{$key} )
                         {
                             if ( ref $config->{$host}->{$key} eq 'ARRAY' )
@@ -319,7 +471,7 @@ sub read_ssh_config
     };
 
     my $error_on_open = open my $fh, q{<}, '/home/' . $user . '/.ssh/config';
-    if ($error_on_open)
+    if ( !$error_on_open )
     {
         die $EXTENDED_OS_ERROR;
     }
@@ -327,14 +479,18 @@ sub read_ssh_config
     {
         $process_ssh_config->( $fh, \%config );
         my $error_on_close = close $fh;
-        if ($error_on_close)
+        if ( !$error_on_close )
         {
             die $EXTENDED_OS_ERROR;
         }
     }
 
-    #print Dumper( \%config );
-    #exit;
+    #~ &dump( \%config );
+    #~ exit;
+
+    #~ &ssh_get_connection_info( \%config, 'zeus.fh-brandenburg.de' );
+    #~ exit;
 
     return \%config;
 }
+
